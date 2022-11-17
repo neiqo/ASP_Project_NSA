339682a9f555fa990053d60db433e3f99813e4fa
Anonymous100k <sehyu2606@outlook.com>
2022-11-17 13:08:53 +0800

    Initial commit

---
+++ Lab3.py
@@ -0,0 +1,94 @@
+#1.
+a = 10
+print(a)
+type(a)
+
+b = 5 + 6j
+print(b)
+type(b)
+(5+6j)
+complex
+
+#3.
+c = True
+print(c)
+type(c)
+
+#4.
+d = 10.5
+print(d)
+type(d)
+
+#5.
+e = 'Testing String'
+print(e)
+type(e)
+
+#6.
+f = [1,2,3]
+print(f)
+type(f)
+
+#7.
+g = (4,5,6)
+print(g)
+type(g)
+
+#8.
+h = {'Student': 'Aaron'}
+print(h)
+type(h)
+
+#9.
+i = {7,8,9}
+print(i)
+type(i)
+
+#10.
+j = [1,2,3]
+k = f+j
+print(k)
+type(k)
+print(1 in k)
+
+#11.
+l = (10,11,12)
+m = l + g
+print(m)
+type(m)
+
+#12.
+k[0] = 11
+print(k)
+#m[0] = 11
+#print[m]
+
+#13.
+n = {'Lectuer': 'Judy'}
+h.update(n)
+print(h)
+type(h)
+
+#14.
+o = {9, 10, 11}
+print(o.union(i))
+type(o)
+o = {9, 10, 11}
+print(o.intersection(i))
+type(o)
+
+#15.
+p = {'AU':'Australia','BE':'Belgium','CN':'China','DK':'Denmark'}
+print(p.values(),p.keys())
+
+#16.
+q = ['Thomas','Linda','Cath','Benny']
+print(sorted(q))
+q.pop()
+print("Names left :", (sorted(q)))
+q.pop()
+print("Names left :", (sorted(q)))
+q.pop()
+print("Names left :", (sorted(q)))
+q.pop()
+print("Names left :", (sorted(q)))
