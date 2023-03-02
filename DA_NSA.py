808eee530c2ba5489301b4e0e0b71458b657a630
Anonymous100k <sehyu2606@outlook.com>
2023-03-02 14:01:36 +0800

    Seiyu

--- DA_NSA.py
+++ DA_NSA.py
@@ -77,5 +77,26 @@ plt.tight_layout()
 print(totalvisitors_top3_df)
 plt.show()
 
+#doing testcase with the help of unittest library
+import unittest
 
+class TestStringMethods(unittest.TestCase):
 
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
+
+#sehyu
